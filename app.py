8b9fcd29f11a03d86c96677a2142064d5545024a
Pirez <gustavo.pires018@gmail.com>
2023-10-26 17:57:31 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Olá, Galera!"
+def login():
+    return render_template('login.html')
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template('cadastro.html')
 
 app.run(debug=True)
